8646bc8ebe00445483e9b5b9ebb95e899b22ff6f
洋輝 大橋 <hiro0084169@outlook.jp>
2025-09-09 12:00:09 +0900

    重複処理削除

--- Project/GameObject/Player/Player.cpp
+++ Project/GameObject/Player/Player.cpp
@@ -430,14 +430,3 @@ Vector3 Player::GetWorldPosition() const
 
 	return worldPos;
 }
-
-Vector3 Player::Get3DWorldPosition()
-{
-	Vector3 worldPos;
-	// ワールド行列の平行移動成分を取得
-	worldPos.x = reticleWorldtransform_.matWorld.m[3][0];
-	worldPos.y = reticleWorldtransform_.matWorld.m[3][1];
-	worldPos.z = reticleWorldtransform_.matWorld.m[3][2];
-
-	return worldPos;
-}
